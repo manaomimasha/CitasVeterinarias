3505f61b64dad009bf9ee953383d9e1fe09bcd94
manaomimasha <manaomi.masha@gmail.com>
2023-04-11 10:45:43 -0300

    fgfg

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,63 +1,66 @@
 import React from 'react'
 
 const Formulario = () => {
-  return (
-    <div className='md:w-1/2 lg:w-2/5'>
+   return (
+      <div className='md:w-1/2 lg:w-2/5'>
 
-      <h1 className='text-black font-bold text-3xl  text-center '>Seguimiento de Pacientes</h1> {""}
-      <p className='text-lg mt-5 text-center'>Añade pacientes y <span span className='text-indigo-600 font-bold'> Administrelos</span></p>
+         <h1 className='text-black font-bold text-3xl  text-center '>Seguimiento de Pacientes</h1> {""}
+         <p className='text-lg mt-5 text-center'>Añade pacientes y <span span className='text-indigo-600 font-bold'> Administrelos</span></p>
 
-      <form action="" className='shadow-md bg-white py-10 px-5 rounded-lg mt-10'>
+         <form action="" className='shadow-md bg-white py-10 px-5 rounded-lg mt-10 mb-10'>
 
-        <label htmlFor="mascota" className='uppercase font-bold text-grey-300 '>  Nombre Mascota</label>
-        <input type="text" id='mascota' className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 mb-5' placeholder=' Nombre de la Mascota' />
+            <label htmlFor="mascota" className='uppercase font-bold text-grey-300 '>  Nombre Mascota</label>
+            <input type="text" id='mascota' className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 mb-5' placeholder=' Nombre de la Mascota' />
 
-        <label htmlFor="prop" 
-           className='uppercase font-bold  text-grey-700 '>  
-           Nombre Propietario
-        </label>
-        <input 
-            type="text" 
-            id='prop' 
-            className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 mb-5' 
-            placeholder=' Su Nombre'
-         />
- <label htmlFor="email" 
-           className='uppercase font-bold  text-grey-700 '>  
-           Email
-        </label>
-        <input 
-            type="email" 
-            id='email' 
-            className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 mb-5 ' 
-            placeholder=' Su Nombre'
-         />
+            <label htmlFor="prop"
+               className='uppercase font-bold  text-grey-700 '>
+               Nombre Propietario
+            </label>
+            <input
+               type="text"
+               id='prop'
+               className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 mb-2'
+               placeholder=' Su Nombre'
+            />
+            <label htmlFor="email"
+               className='uppercase font-bold  text-grey-700 '>
+               Email
+            </label>
+            <input
+               type="email"
+               id='email'
+               className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 mb-5 '
+               placeholder=' Su Nombre'
+            />
 
-        <label htmlFor="alta" 
-           className='uppercase font-bold  text-grey-700 '>  
-           Alta
-        </label>
-        <input 
-            type="date" 
-            id='alta' 
-            className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2  mb-5' 
-            placeholder=' Su Nombre'
-         />
+            <label htmlFor="alta"
+               className='uppercase font-bold  text-grey-700 '>
+               Alta
+            </label>
+            <input
+               type="date"
+               id='alta'
+               className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2  mb-5'
+               placeholder=' Su Nombre'
+            />
 
-<label htmlFor="email" 
-           className='uppercase font-bold  text-grey-700 '>  
-           Describe los Sintomas
-        </label>
-        <input 
-            type="textarea" 
-            id='email' 
-            className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 ' 
-            placeholder='Describe los Sintomas'
-         />
+            <label htmlFor="email"
+               className='uppercase font-bold  text-grey-700 '>
+               Describe los Sintomas
+            </label>
+            <input
+               type="textarea"
+               id='email'
+               className='w-full border-2 p-2 placeholder-gray-400 rounded-md mt-2 '
+               placeholder='Describe los Sintomas'
+            />
 
-      </form>
-    </div>
-  )
+            <input type="submit"
+               className='p-3 w-full text-indigo-600  hover:bg-indigo-700 transition-colors text-white uppercase font-bold cursor-pointer bg-indigo-600 mb-2 mt-5 '
+               value={"Agregar Paciente"} />
+         </form>
+      </div>
+   )
 }
 
 export default Formulario
