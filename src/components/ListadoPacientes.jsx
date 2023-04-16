852d671f9ef1d214e21a8a22560023bdecd4747c
manaomimasha <manaomi.masha@gmail.com>
2023-04-16 18:58:19 -0300

    yes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,15 +1,28 @@
 import Paciente from './Paciente'
 
-const ListadoPacientes = ( {pacientes}) => {
+const ListadoPacientes = ({ pacientes }) => {
 
+  console.log(pacientes)
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
       <h1 className=' text-black font-bold text-3xl  text-center '>Listado de Pacientes</h1>
       <p className=' text-xl text-center mt-5 mb-10'> {""}
         Administra tus  <span className='font-bold text-indigo-600 mb-5'> Pacientes y Citas</span></p>
+
+
 {
-  pacientes.map( pac => )
+  pacientes.map( (pacientE)=> 
+       (
+        <Paciente
+        pacientE= {pacientE}
+          />
+      )
+    
+     
+  )
 }
+
+     
 
     </div>
   )
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,25 +1,33 @@
- const Paciente =  () => {
+ const Paciente =  ({pacientE}) => {
 
-   const {nombre, propietario, email, fecha, sintomas} = pac
-
+ const {nombre, propietario, email, fecha, sintomas} = pacientE
+console.log(pacientE)
  return (
   <div className=" bg-white shadow-md rounded-xl m-3 px-5 py-10 ">
 
        <p className="uppercase text-grey-700 mb-3 font-bold "> Nombre mascota:  {""}
-         <span className="font-normal normal-case"> {nombre} </span>
+         <span className="font-normal normal-case"> {nombre}
+         </span>
       </p>
       
       <p className="uppercase text-grey-700 mb-3 font-bold "> Propietario:  {""}
-      <span className="font-normal normal-case"> {propietario} </span>   </p>
+      <span className="font-normal normal-case"> {propietario}
+       </span>   </p>
 
       <p className="uppercase text-grey-700 mb-3 font-bold "> Email:  {""}
-         <span className="font-normal normal-case">{email} </span>      </p>
+         <span className="font-normal normal-case">
+          {email}
+          </span>      </p>
 
    <p className="uppercase text-grey-700 mb-3 font-bold "> Fecha Alta:  {""}
-      <span className="font-normal normal-case"> {alta} </span>   </p>
+      <span className="font-normal normal-case"> 
+      {fecha} 
+      </span>   </p>
 
      <p className="uppercase text-grey-700 mb-3 font-bold "> Sintomas:  {""}
-        <span className="font-normal normal-case"> {sintomas} </span>
+        <span className="font-normal normal-case"> 
+        {sintomas} 
+         </span>
        </p>
 
     </div>
