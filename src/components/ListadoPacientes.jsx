6a1be56047ef718eb9f6dec0f9c0a4ded317f097
manaomimasha <123413831+manaomimasha@users.noreply.github.com>
2023-04-16 16:04:20 -0300

    dsds

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -8,11 +8,11 @@ const ListadoPacientes = ( {pacientes}) => {
       <p className=' text-xl text-center mt-5 mb-10'> {""}
         Administra tus  <span className='font-bold text-indigo-600 mb-5'> Pacientes y Citas</span></p>
 
-      { pacientes.map ( (pac) => {
+      { pacientes.map ( (pac) => (
         <Paciente 
         pac= {pac}
         />
-      })}
+      ))}
       
 
 
