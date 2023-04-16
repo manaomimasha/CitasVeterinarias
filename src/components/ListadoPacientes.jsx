38d61666ac106b771422b3284d9f990d86668685
manaomimasha <123413831+manaomimasha@users.noreply.github.com>
2023-04-16 13:31:45 -0300

    lala

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from './Paciente'
 
-const ListadoPacientes = ( ) => {
+const ListadoPacientes = ( {pacientes}) => {
 
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
@@ -8,9 +8,14 @@ const ListadoPacientes = ( ) => {
       <p className=' text-xl text-center mt-5 mb-10'> {""}
         Administra tus  <span className='font-bold text-indigo-600 mb-5'> Pacientes y Citas</span></p>
 
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
+      { pacientes.map ( pacientE => {
+        <Paciente 
+
+        pacientE= {pacientE}
+        
+        
+        />
+      })}
 
 
     </div>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,9 +1,12 @@
 
- const Paciente =  () => {
-    
+ const Paciente =  ({pacientE}) => {
+
+    const {nombre, propietario,fecha, sintomas} = pacientE
+
  return (
 
   <div className=" bg-white shadow-md rounded-xl m-3 px-5 py-10 ">
+
        <p className="uppercase text-grey-700 mb-3 font-bold "> Nombre mascota:  {""}
          <span className="font-normal normal-case"> {nombre}</span>
       </p>
@@ -12,9 +15,9 @@
       <span className="font-normal normal-case"> {propietario}</span>   </p>
 
       <p className="uppercase text-grey-700 mb-3 font-bold "> Email:  {""}
-         <span className="font-normal normal-case"> {email}</span>      </p>
+         <span className="font-normal normal-case"> lala@lala.com </span>      </p>
 
-  <p className="uppercase text-grey-700 mb-3 font-bold "> Fecha Alta:  {""}
+   <p className="uppercase text-grey-700 mb-3 font-bold "> Fecha Alta:  {""}
       <span className="font-normal normal-case">{fecha}</span>   </p>
 
      <p className="uppercase text-grey-700 mb-3 font-bold "> Sintomas:  {""}
