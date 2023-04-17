686b7e2d9098f21b3cf956b35bd8412806410082
manaomimasha <123413831+manaomimasha@users.noreply.github.com>
2023-04-17 12:26:13 -0300

    2

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,13 +1,7 @@
 import Paciente from './Paciente'
 
-import { useEffect } from "react"
-
-const ListadoPacientes = ({ pacientes, setPaciente, paciente }) => {
+const ListadoPacientes = ({ pacientes, setPaciente }) => {
   
-     useEffect(() => {
-        if (pacientes.length > 0) { console.log('NuevoPaciente') }
-     }, [pacientes])
-
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
 
@@ -23,7 +17,6 @@ const ListadoPacientes = ({ pacientes, setPaciente, paciente }) => {
             Añade pacientes <span className='font-bold text-indigo-600 mb-5'> y aparecerán aquí </span></p>
 
         </>
-
       }
 
       {
